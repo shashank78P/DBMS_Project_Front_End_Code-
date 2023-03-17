923fb5c254ca8c85e635c940d4032e51e849aba5
Shashank P <96535169+shashank78P@users.noreply.github.com>
2023-03-17 11:50:47 +0530

    project

---
+++ src/PrivateRoute/private.js
@@ -0,0 +1,8 @@
+// import { Navigate } from "react-router-dom";
+
+// const Private=({children})=>{
+//     const [jwt,setjwt] = useLocalStorage("","jwt");
+//     return jwt ? children : <Navigate to="/login"/>;
+// };
+
+// export default Private;
---
+++ src/loader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loader=()=>{
+    return(
+        <div>
+            <h1 style={{display:'block',margin:"auto"}}>Loading</h1>
+        </div>
+    )
+}
+
+export default Loader;
